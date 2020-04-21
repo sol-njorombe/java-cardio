9379a9e26438495fb03273f6f5ec78a80faaabb4
NJOROMBE <njorombe@gmail.com>
2020-04-20 21:25:43 -0700

    initial commit

---
+++ BUILD
@@ -0,0 +1,6 @@
+load("@rules_java//java:defs.bzl", "java_binary")
+
+java_binary(
+  name = "Starter",
+  srcs = glob(["src/main/java/com/algorithms/*.java"]),
+)
---
+++ src/main/java/com/algorithms/Starter.java
@@ -0,0 +1,7 @@
+package com.algorithms;
+
+public class Starter {
+  public static void main(String args[]) {
+    System.out.println("This is the beginning of the application");
+  }
+}
